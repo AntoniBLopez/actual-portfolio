7fd4dbe9482d8e6110c257a001a2d9828b50f3d3
ToniBLopez <toniblopezdeveloper@gmail.com>
2024-04-23 01:25:58 +0200

    My simplified react-i18next

---
+++ src/translate/Languages.tsx
@@ -0,0 +1,12 @@
+
+const Languages = {
+  es: {
+    profession: 'Desarrollador de Software Frontend'
+  },
+  en: {
+    profession: 'Frontend Software Developer'
+  }
+}
+
+
+export default Languages
