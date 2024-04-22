744d937b0dc152557fb735b3680cded37650add4
ToniBLopez <toniblopezdeveloper@gmail.com>
2024-04-23 01:21:59 +0200

    customized colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 export default {
+  darkMode: 'class',
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
@@ -9,6 +10,12 @@ export default {
     extend: {
       dropShadow: {
         '3xl': '0px 0px 10px #0061FF',
+      },
+      textColor: {
+        'primaryDark': '#81bbfc',
+        'primaryLight': '#3490dc',
+        'secondary': '#ffed4a',
+        'danger': '#e3342f',
       }
     },
   },
