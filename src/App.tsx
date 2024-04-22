d03cf8dc35d883e663dd524c9ceb1f22542f6084
ToniBLopez <toniblopezdeveloper@gmail.com>
2024-04-23 01:22:27 +0200

    Changing theme settings

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,22 @@ import MyCvWeb from './pages/MyCvWeb'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 function App() {
+  // const mode = localStorage.getItem("mode")
 
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          {/* <Route path='/' element={<Home />} /> */}
-          <Route path='/' element={<Navigate to='/cv' />} />
-          <Route path='/cv' element={<MyCvWeb />} />
-          {/* <Route path='/home'>
+      // <div className={mode}>
+        <BrowserRouter>
+          <Routes>
+            {/* <Route path='/' element={<Home />} /> */}
+            <Route path='/' element={<Navigate to='/cv' />} />
+            <Route path='/cv' element={<MyCvWeb />} />
+            {/* <Route path='/home'>
             <Route index element={isAuth ? <HomePage /> : <Navigate to='/' />} />
             <Route path='profile/:userId' element={isAuth ? <ProfilePage /> : <Navigate to='/' />} />
           </Route> */}
-        </Routes>
-      </BrowserRouter>
-    </>
+          </Routes>
+        </BrowserRouter>
+      // </div>
   )
 }
 
